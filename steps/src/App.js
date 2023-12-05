13458a116be2ec6e751fd4d1eb6d2d19ea532393
appu-46 <ap958461@gmail.com>
2023-12-05 14:14:26 +0530

    implemented button functionality

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,17 +1,21 @@
+import { useState } from "react";
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
-  "Invest your new income 💰",
+  "❓❓❓ ",
+  "Profit 🤑🤑🤑",
 ];
 export default function App() {
-  let step = 1;
-
+  const [step, setStep] = useState(1);
+  // console.log(arr);
+  // const step = 1;
   function handlePrevious() {
-    alert("previous");
+    setStep(step - 1);
+    // alert("previous");
   }
 
   function handleNext() {
-    alert("Next");
+    setStep(step + 1);
   }
   return (
     <div className="steps">
@@ -19,6 +23,7 @@ export default function App() {
         <div className={`${step >= 1 ? "active" : ""}`}>1</div>
         <div className={`${step >= 2 ? "active" : ""}`}>2</div>
         <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={`${step >= 4 ? "active" : ""}`}>4</div>
       </div>
 
       <p className="message">
@@ -29,14 +34,16 @@ export default function App() {
         <button
           style={{ backgroundColor: "#7950f2", color: "#fff" }}
           onClick={handlePrevious}
+          // onMouseEnter={() => alert("test")}
+          // onClick={() => alert("TEST")}
         >
-          Previous
+          ⬅️ Previous
         </button>
         <button
           style={{ backgroundColor: "#7950f2", color: "#fff" }}
           onClick={handleNext}
         >
-          Next
+          Next ➡️
         </button>
       </div>
     </div>
