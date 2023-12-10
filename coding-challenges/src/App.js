8a750fcdfefbffe68f7e37b48aa5328c09bf079b
appu-46 <ap958461@gmail.com>
2023-12-10 21:26:38 +0530

    coding challenge

--- coding-challenges/src/App.js
+++ coding-challenges/src/App.js
@@ -109,3 +109,62 @@ function FlashCards() {
   );
 }
 */
+
+// Coding Challenge 3 : Date counter V2
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  function handleSlider() {}
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
+  return (
+    <div>
+      <input
+        type="range"
+        min={0}
+        max={10}
+        value={Number(step)}
+        onChange={(e) => setStep(Number(e.target.value))}
+      />
+      Step : {step}
+      {/* <div>
+        <button onClick={() => setStep((s) => s - 1)}> - </button>
+        Step: {step}
+        <button onClick={() => setStep((s) => s + 1)}> + </button>
+      </div> */}
+      <div>
+        <button onClick={() => setCount((c) => c - step)}> - </button>
+        <input type="text" placeholder={count} value={count} />
+        <button onClick={() => setCount((c) => c + step)}> + </button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+      <div>
+        <button onClick={handleReset}> Reset </button>
+      </div>
+    </div>
+  );
+}
