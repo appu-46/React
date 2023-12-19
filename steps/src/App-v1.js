20aae84ef419775221d24a8ea0dce1fb36a6b18a
appu-46 <ap958461@gmail.com>
2023-12-19 19:31:08 +0530

    code sync

--- steps/src/App-v1.js
+++ steps/src/App-v1.js
@@ -32,43 +32,28 @@ export default function App() {
             <div className={step >= 4 ? "active" : ""}>4</div>
           </div>
 
-          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
 
           <div className="buttons">
-            <Button
-              bgColor={"#7950f2"}
-              textColor={"#fff"}
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handlePrevious}
+              // onMouseEnter={() => alert("test")}
+              // onClick={() => alert("TEST")}
             >
-              <span>⬅️</span> Previous
-            </Button>
-
-            <Button bgColor={"#7950f2"} textColor={"#fff"} onClick={handleNext}>
-              Next <span>➡️ </span>
-            </Button>
+              ⬅️ Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next ➡️
+            </button>
           </div>
         </div>
       )}
     </>
-  );
-}
-
-function StepMessage({ step, children }) {
-  return (
-    <div className="message">
-      <h3>Step {step}</h3>
-      {children}
-    </div>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
