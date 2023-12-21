eb3c0dd1d8d50699526c93644381ba207dcbab84
appu-46 <ap958461@gmail.com>
2023-12-22 01:00:15 +0530

    star rating selection implemention

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+
+import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      color={"gold"}
+      size={48}
+      messages={["terrible", "bad", "okay", "good", "amazing"]}
+      maxRating={5}
+    />
+    <StarRating
+      color={"red"}
+      size={64}
+      messages={["terrible", "bad", "okay", "good", "amazing"]}
+      maxRating={5}
+    />
   </React.StrictMode>
 );
