bda6e163045d82fdbeaed87be0b27b07c0143398
appu-46 <ap958461@gmail.com>
2023-12-25 21:49:08 +0530

    implemented movie details

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import StarRating from "./StarRating";
 
 const tempMovieData = [
   {
@@ -92,6 +93,7 @@ function Search({ query, setQuery }) {
 function Main({ children }) {
   return <main className="main">{children}</main>;
 }
+
 function MovieList({ movies, selectedId, handleSelectedMovie }) {
   return (
     <ul className="list  list-movies">
@@ -228,12 +230,73 @@ function Errormessage({ message }) {
   );
 }
 function MovieDetails({ selectedId, onCloseMovie }) {
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+  console.log(title, year, poster);
+  useEffect(
+    function () {
+      async function getMovieDetails() {
+        setIsLoading(true);
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+        );
+        const data = await res.json();
+        setMovie(data);
+        console.log(data);
+        setIsLoading(false);
+      }
+      getMovieDetails();
+    },
+    [selectedId]
+  );
   return (
     <div className="details">
-      <button className="btn-back" onClick={onCloseMovie}>
-        &larr;
-      </button>
-      {selectedId}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <header>
+            <button className="btn-back" onClick={onCloseMovie}>
+              &larr;
+            </button>
+            <img src={poster} alt={`Poster of ${title} movie`} />
+            <div className="details-overview">
+              <h2>{title}</h2>
+              <p>
+                {released} &bull; {runtime}
+              </p>
+              <p>{genre}</p>
+              <p>
+                <span>⭐</span>
+                {imdbRating} IMDb Rating
+              </p>
+            </div>
+          </header>
+          <section>
+            <div className="rating">
+              <StarRating maxRating={10} size={26} />
+            </div>
+            <p>
+              <em>{plot}</em>
+            </p>
+            <p>Starring: {actors}</p>
+            <p>Directed by: {director}</p>
+          </section>
+        </>
+      )}
     </div>
   );
 }
@@ -245,7 +308,6 @@ export default function App() {
   const [error, setError] = useState("");
   const [watched, setWatched] = useState(tempWatchedData);
   const [selectedId, setselectedId] = useState(null);
-  const tempquery = "interstellar";
 
   function handleSelectedMovie(id) {
     setselectedId((selectedId) => (selectedId === id ? null : id));
