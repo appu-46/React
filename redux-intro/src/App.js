ce9e63dbbe9badcbfa6c25066f4d6dc397f3f3f8
appu-46 <ap958461@gmail.com>
2024-03-23 20:51:58 +0530

    implementing middleware

--- redux-intro/src/App.js
+++ redux-intro/src/App.js
@@ -5,7 +5,7 @@ import BalanceDisplay from "./features/accounts/BalanceDisplay";
 import { useSelector } from "react-redux";
 
 function App() {
-  const fullName = useSelector((state) => state.Customer.fullName);
+  const fullName = useSelector((state) => state.customer.fullName);
   return (
     <div>
       <h1>🏦 The React-Redux Bank ⚛️</h1>
