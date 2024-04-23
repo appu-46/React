f920189ddcf82450457bbe1767a91348e8287f70
appu-46 <ap958461@gmail.com>
2024-04-23 22:16:03 +0530

    trying tailwind properties

---
+++ fast-react-pizza/src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="hidden text-sm font-semibold">Appu</div>;
+}
+
+export default Username;
--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -1,15 +1,16 @@
 import SearchOrder from "../features/order/SearchOrder";
 import { Link } from "react-router-dom";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <div className="uppercase bg-yellow-500">
+    <div className="px-3 py-4 uppercase bg-yellow-500 border-b border-stone-500 sm:px-6">
       <Link to="/" className="tracking-[0.3rem]">
         Fast React Pizza Co.
       </Link>
       <SearchOrder />
 
-      <p>Pija</p>
+      <Username />
     </div>
   );
 }
