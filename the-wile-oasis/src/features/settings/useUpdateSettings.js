12c3ba8a989c386b05efc8e428e67ae435c41b5f
appu-46 <ap958461@gmail.com>
2025-04-13 19:58:29 +0530

    code sync

--- the-wile-oasis/src/features/settings/useUpdateSettings.js
+++ the-wile-oasis/src/features/settings/useUpdateSettings.js
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-
+import toast from "react-hot-toast";
 import { updateSetting as updateSettingAPI } from "../../services/apiSettings";
 
 export function useUpdateSettings() {
@@ -11,9 +11,12 @@ export function useUpdateSettings() {
     error,
   } = useMutation({
     mutationFn: updateSettingAPI,
-    onSuccess: queryClient.invalidateQueries({
-      queryKey: ["settings"],
-    }),
+    onSuccess: () => {
+      toast.success("Settings updated successfully!");
+      queryClient.invalidateQueries({
+        queryKey: ["settings"],
+      });
+    },
   });
 
   return { isUpdating, updateSettings, error };
