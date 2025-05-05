1eac55f030dd195b88f29de40696c9f726235da4
appu-46 <ap958461@gmail.com>
2025-05-05 08:09:59 +0530

    updated font

--- the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -33,21 +33,21 @@ const Cabin = styled.div`
   font-size: 1.6rem;
   font-weight: 600;
   color: var(--color-grey-600);
-  font-family: "Sono";
+  font-family: "Georama";
 `;
 
 const Price = styled.div`
-  font-family: "Sono";
+  font-family: "Georama";
   font-weight: 600;
 `;
 
 const Capacity = styled.div`
-  font-family: "Sono";
+  font-family: "Georama";
   font-weight: 600;
 `;
 
 const Discount = styled.div`
-  font-family: "Sono";
+  font-family: "Georama";
   font-weight: 500;
   color: var(--color-green-700);
 `;
