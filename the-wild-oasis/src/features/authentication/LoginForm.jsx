235f13f0256d1d96ab466bb22d7c9cd58643bf50
appu-46 <ap958461@gmail.com>
2025-06-15 23:00:41 +0530

    Authentication implemented baby

--- the-wild-oasis/src/features/authentication/LoginForm.jsx
+++ the-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -2,17 +2,26 @@ import { useState } from "react";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
-import FormRowVertical from "../../ui/FormRowVertical";
+import FormRow from "../../ui/FormRow";
+import { useLogin } from "./useLogin";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("appu@example.com");
+  const [password, setPassword] = useState("Chess@456");
 
-  function handleSubmit() {}
+  const { login, isLoggingIn } = useLogin();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !password) return;
+
+    login({ email, password });
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
-      <FormRowVertical label="Email address">
+      <FormRow orientation="vertical" label="Email address">
         <Input
           type="email"
           id="email"
@@ -20,20 +29,22 @@ function LoginForm() {
           autoComplete="username"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={isLoggingIn}
         />
-      </FormRowVertical>
-      <FormRowVertical label="Password">
+      </FormRow>
+      <FormRow orientation="vertical" label="Password">
         <Input
           type="password"
           id="password"
           autoComplete="current-password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={isLoggingIn}
         />
-      </FormRowVertical>
-      <FormRowVertical>
-        <Button size="large">Login</Button>
-      </FormRowVertical>
+      </FormRow>
+      <FormRow>
+        <Button size="large">{!isLoggingIn ? "Login" : <SpinnerMini />}</Button>
+      </FormRow>
     </Form>
   );
 }
---
+++ the-wild-oasis/src/features/authentication/useLogin.js
@@ -0,0 +1,23 @@
+import { useMutation } from "@tanstack/react-query";
+import { login as loginApi } from "../../services/apiAuth";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+export function useLogin() {
+  const navigate = useNavigate();
+
+  const { mutate: login, isPending: isLoggingIn } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: (userObject) => {
+      console.log(userObject);
+      toast.success(`Successfully logged in ${userObject.user.email}!`);
+      navigate("/dashboard");
+    },
+    onError: (err) => {
+      toast.error(`Password and the email are incorrect. Please try again!`);
+      console.log(err);
+    },
+  });
+
+  return { login, isLoggingIn };
+}
